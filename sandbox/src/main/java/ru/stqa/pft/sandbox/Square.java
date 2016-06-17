bed51051870e4b00c8a3700a6782b6098530b1f5
NatkinP <natkin_p@list.ru>
2016-06-17 20:41:44 +0300

    Реализованы методы и классы

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by natkin on 17.06.2016.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double len){
+    this.l = len;
+  }
+
+  public double area(){
+    return this.l * this.l;
+  }
+
+
+}
