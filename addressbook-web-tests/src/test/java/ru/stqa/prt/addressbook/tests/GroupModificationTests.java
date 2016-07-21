31c9b420991a94de297f8c04b80fba3981b466c3
NatkinP <natkin_p@list.ru>
2016-07-21 22:00:17 +0300

    Добавлен метод модификации группы
    Класс GroupModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/prt/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.prt.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.prt.addressbook.model.GroupData;
+
+/**
+ * Created by natkin on 21.07.2016.
+ */
+public class GroupModificationTests extends TestBase{
+  @Test
+  public void testGroupModification(){
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+
+}
