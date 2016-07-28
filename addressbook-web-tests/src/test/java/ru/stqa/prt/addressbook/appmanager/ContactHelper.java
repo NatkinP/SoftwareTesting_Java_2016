d971fa6f6dc36757ba5f9a91f710edeedce20851
NatkinP <natkin_p@list.ru>
2016-07-28 16:22:06 +0300

    Реализован if при запуске браузера

--- addressbook-web-tests/src/test/java/ru/stqa/prt/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/prt/addressbook/appmanager/ContactHelper.java
@@ -1,6 +1,7 @@
 package ru.stqa.prt.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.prt.addressbook.model.ContactCompanyParam;
@@ -13,7 +14,7 @@ import ru.stqa.prt.addressbook.model.ContactPhones;
 public class ContactHelper extends HelperBase {
 
 
-  public ContactHelper(FirefoxDriver wd) {
+  public ContactHelper(WebDriver wd) {
     super(wd);
   }
 
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equality.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by natkin on 28.07.2016.
+ */
+public class Equality {
+
+  public static void main (String[] args){
+    String s1 = "firefox";
+    String s2 = new String(s1);
+
+
+    System.out.println(s1 == s2);
+    System.out.println(s1.equals(s2));
+  }
+}
