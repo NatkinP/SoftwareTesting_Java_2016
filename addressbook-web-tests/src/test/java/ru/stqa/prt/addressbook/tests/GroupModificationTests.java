0aa883d53ac65f3aecdf1dc46ee48f0433927bb0
NatkinP <natkin_p@list.ru>
2016-08-01 17:18:52 +0300

    Группы: проверки со счетчика элементов заменены на счетчик размера коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/prt/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/prt/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.prt.addressbook.model.GroupData;
 
+import java.util.List;
+
 /**
  * Created by natkin on 21.07.2016.
  */
@@ -14,14 +16,14 @@ public class GroupModificationTests extends TestBase{
     if (! app.getGroupHelper().isThereAgroup()){
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
-    int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroup();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
   }
 
 }
