24c684a1861c934e9ff483f2542fea0f262dfe80
NatkinP <natkin_p@list.ru>
2016-08-05 19:07:06 +0300

    Освоено логирование

--- addressbook-web-tests/src/test/java/ru/stqa/prt/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/prt/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,8 @@
 package ru.stqa.prt.addressbook.tests;
 
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.prt.addressbook.model.GroupData;
